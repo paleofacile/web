de3d3912235180d4f3cbc2045165758ebf9c6c86
Sami Dalouche <sami.dalouche@gmail.com>
2013-05-04 17:32:45 -0400

    video 2

---
+++ videos/2-commencer-paleo/presentation/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/presentation/d/1qd1E4i74Wwp5dCYezPr1pMQnQP_-oNIvcIfxUjlIBgY");
+?>
+
---
+++ videos/2-commencer-paleo/references/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/document/d/1Bd8-4Rw86L7-76PoSjb-Qv3ksOMjQ2JFJMOPvITqHik");
+?>
+
