65b67d42f3ed09eba654b8085a9553a3965025e4
Sami Dalouche <sami.dalouche@gmail.com>
2013-05-06 22:59:02 -0400

    3 - special questions reponses

---
+++ videos/3-special-questions-reponses/presentation/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/presentation/d/1I6OFmtc9WZ13TpG4S8XY8hjUnh9PDPL-VDio0meptHk");
+?>
+
---
+++ videos/3-special-questions-reponses/references/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/document/d/1G-3HMPgP4YqhM-TBKijIRYi4J-20tjUX0jZLC0j4jgw");
+?>
+
