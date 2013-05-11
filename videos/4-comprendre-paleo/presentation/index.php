29734f8dfd1a26e9831816b5f5c6e65b670ededf
Sami Dalouche <sami.dalouche@gmail.com>
2013-05-11 17:43:57 -0400

    4-comprendre-paleo

---
+++ videos/4-comprendre-paleo/presentation/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/presentation/d/1-OtRyHEzSqvtvtXpik6NegBWb8eFJEYu7lACO7S1--U/");
+?>
+
---
+++ videos/4-comprendre-paleo/references/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/document/d/19V3dZRWfrc9RpHVcF7IUDuemo9Axfs5VNLXb2oHNE5g");
+?>
+
