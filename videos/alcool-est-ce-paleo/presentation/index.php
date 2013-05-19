cd101d4bdf3fabd9bd1030e31c8b19d933efda67
Sami Dalouche <sami.dalouche@gmail.com>
2013-05-19 11:34:05 -0400

    alcool: est-ce paleo

---
+++ videos/alcool-est-ce-paleo/presentation/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/presentation/d/1pUG6Zn7nsKaFuOSO-PswYBb4hhR90Ceitn2o9Kq9cYk");
+?>
+
---
+++ videos/alcool-est-ce-paleo/references/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/document/d/1_yYVPFYZXT7Q-4Z7diDZ2Ak52BLNM2HOznlXQxCMTxA");
+?>
+
