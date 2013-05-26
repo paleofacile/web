b305cf0edfcc6f306a47e1a70162db6e911d1766
Sami Dalouche <sami.dalouche@gmail.com>
2013-05-26 16:50:42 -0400

    produits laitiers

---
+++ videos/produits-laitiers-est-ce-paleo/presentation/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/presentation/d/1A_J_SmCmvZg3rLWH-fqu1ITtHu-r50iBKVIgUoaADbM");
+?>
+
---
+++ videos/produits-laitiers-est-ce-paleo/references/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/document/d/1KfSHUOiauAJfR2cNSNKTkb9gBChlDX5qJ42w1aJk9fg");
+?>
+
