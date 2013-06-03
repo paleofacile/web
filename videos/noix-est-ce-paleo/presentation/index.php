be9a74b929d2a28471098272c6f1b515292853f4
Sami Dalouche <sami.dalouche@gmail.com>
2013-06-02 23:05:22 -0400

    noix - est ce paleo

---
+++ videos/noix-est-ce-paleo/presentation/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/presentation/d/1iBDxqU0-rBGXRfEIuzSCHvjic8HI__S1O8RaFimFbSM");
+?>
+
---
+++ videos/noix-est-ce-paleo/references/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/document/d/1BS5rhOUxQEq01HCfRzEn9tbHfKQE9nAeX8mgFu4bvGM");
+?>
+
