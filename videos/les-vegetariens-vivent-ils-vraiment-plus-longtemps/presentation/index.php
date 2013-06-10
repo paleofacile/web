e52a56181a3b67ec36e8128a436e98b1940156cc
Sami Dalouche <sami.dalouche@gmail.com>
2013-06-09 20:10:53 -0400

    les-vegetariens-vivent-ils-vraiment-plus-longtemps/

---
+++ videos/les-vegetariens-vivent-ils-vraiment-plus-longtemps/presentation/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/presentation/d/1jRNnSHXCmnqGwA1FPCsTgl4TBopJo-ZxujieztsOuSY");
+?>
+
---
+++ videos/les-vegetariens-vivent-ils-vraiment-plus-longtemps/references/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/document/d/1doJuQkxgXfjbU7yGsixGPcuz0-wWfA-feqCAXsJ_RBM");
+?>
+
