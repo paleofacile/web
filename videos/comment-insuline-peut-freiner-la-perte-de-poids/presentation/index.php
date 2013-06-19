4ec3d9e57085d00644ad3df28b93d7249925acce
Sami Dalouche <sami.dalouche@gmail.com>
2013-06-19 18:48:27 -0400

    Comment l'insuline peut freiner la perte de poids

---
+++ videos/comment-insuline-peut-freiner-la-perte-de-poids/presentation/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/presentation/d/1opvoOO9lVxpDwH_HcwGDNSU8CvO4mDQ3w7UlIc6iwn4");
+?>
+
---
+++ videos/comment-insuline-peut-freiner-la-perte-de-poids/references/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/document/d/1XsnVRsogJYOEMeQ9tpjKIOvL4snqcgqswqOxsSMLuVc");
+?>
+
