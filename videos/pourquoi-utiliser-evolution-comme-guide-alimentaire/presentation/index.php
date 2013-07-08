f952cb80c2346564f9721d0aac694c0bd23f732a
Sami Dalouche <sami.dalouche@gmail.com>
2013-07-07 21:06:03 -0400

    pourquoi utiliser l'evolution comme guide alimentaire

---
+++ videos/pourquoi-utiliser-evolution-comme-guide-alimentaire/presentation/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/presentation/d/1FINAjd15SAz_hMclJ28utln1nMOeuDAwAAqDQ_BzH6E");
+?>
+
---
+++ videos/pourquoi-utiliser-evolution-comme-guide-alimentaire/references/index.php
@@ -0,0 +1,4 @@
+<?php
+Header("Location: https://docs.google.com/document/d/1Bqr0T0T4klh18sRKj7SGJYSMoGLNwebay43QEfdApes");
+?>
+
